5f6dde0c7e832631d0918d5b48cc1c300299607d
vnemala <109118507+vnemala@users.noreply.github.com>
2023-04-09 17:35:26 -0400

    update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ classifier = pipeline("sentiment-analysis")
 text = st.text_input('Enter sample text', 'I really like HuggingFace and it is a great tool for deploying AI models.')
 
 result = classifier(text)
-st.write('The sentiment displayed in the sample text is', result[0]['label'], 'with a', str((result[0]['score'])*100 + ' %'), 'accuracy')
+st.write('The sentiment displayed in the sample text is', result[0]['label'], 'with a', (str((result[0]['score'])*100) + ' %'), 'accuracy')
