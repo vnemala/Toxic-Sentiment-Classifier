f70f4ec7300ae0d61aad5efd20b4a01494351137
vnemala <109118507+vnemala@users.noreply.github.com>
2023-04-09 15:46:48 -0400

    update

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 
 
 from transformers import pipeline
-import torch
-import torch.nn.functional as functional
 
 classifier = pipeline("sentiment-analysis")
 text = st.text_input('Enter sample text', 'I really like HuggingFace and it is a great tool for deploying AI models.')
