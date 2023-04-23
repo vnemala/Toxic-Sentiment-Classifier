25200a7634aa7ae785fa64151c1a0f63410dae5b
vnemala <109118507+vnemala@users.noreply.github.com>
2023-04-23 17:51:44 -0400

    Update toxic_model.py

--- toxic_model.py
+++ toxic_model.py
@@ -1,1 +1,13 @@
 
+import streamlit as st
+from transformers import pipeline
+
+st.title("Toxic Comment Analysis")
+
+model_name = 'sentiment-analysis'
+classifier = pipeline(model_name)
+
+text = st.text_input('Enter sample text:', 'I really like HuggingFace and it is a great tool for deploying AI models.')
+
+result = classifier(text)
+st.write('The sentiment displayed in the sample text is', result[0]['label'], 'with a', (str((result[0]['score'])*100) + ' %'), 'certainty.')
