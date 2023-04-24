03e66ea95a320e831abf22b02bf57c2e4fec63f0
vnemala <109118507+vnemala@users.noreply.github.com>
2023-04-23 23:38:56 -0400

    Update toxic_model.py

--- toxic_model.py
+++ toxic_model.py
@@ -4,10 +4,24 @@ from transformers import pipeline
 
 st.title("Toxic Comment Analysis")
 
-model_name = 'sentiment-analysis'
-classifier = pipeline(model_name)
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-text = st.text_input('Enter sample text:', 'I really like HuggingFace and it is a great tool for deploying AI models.')
+import torch
+import torch.nn.functional as F
 
-result = classifier(text)
-st.write('The sentiment displayed in the sample text is', result[0]['label'], 'with a', (str((result[0]['score'])*100) + ' %'), 'certainty.')
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+
+tokenizer = AutoTokenizer.from_pretrained('toxic-comment-model')
+model = AutoModelForSequenceClassification.from_pretrained('toxic-comment-model')
+
+classifier = pipeline("text-classification",model = model, tokenizer = tokenizer)
+
+test_df = pd.read_csv('test.csv')
+
+list_test = test_df['comment_text'].tolist()[0:3]
+results = classifier(list_test)
+
+st.write('The sentiment displayed in the sample text is', results[0]['label'], 'with a', (str((results[0]['score'])*100) + ' %'), 'certainty.')
+st.write('The sentiment displayed in the sample text is', results[1]['label'], 'with a', (str((results[1]['score'])*100) + ' %'), 'certainty.')
+st.write('The sentiment displayed in the sample text is', results[2]['label'], 'with a', (str((results[2]['score'])*100) + ' %'), 'certainty.')
